1379f0a737088d378997e1005857c91faa7393e8
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-03 16:53:31 +0000

    Version 1.2.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fluid_recommendation/tags/1_2_1@64200 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fluid_recommendation".
 #
-# Auto generated 20-01-2012 10:51
+# Auto generated 03-07-2012 17:38
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,36 +14,38 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Fluid Recommendation',
 	'description' => 'Let your visitors recommend your website by mail. Based on Fluid Templating Engine.',
 	'category' => 'plugin',
-	'author' => 'Armin Ruediger Vieweg',
-	'author_email' => 'info@professorweb.de',
-	'author_company' => '',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '1.2.1',
 	'dependencies' => 'extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
-	'version' => '1.2.0',
+	'author' => 'Armin Ruediger Vieweg',
+	'author_email' => 'armin@v.ieweg.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-0.0.0',
-			'extbase' => '1.3.0-1.5.99',
-			'fluid' => '1.3.0-1.5.99',
+			'extbase' => '1.3.0-0.0.0',
+			'fluid' => '1.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
+	'_md5_values_when_last_written' => 'a:24:{s:21:"ext_conf_template.txt";s:4:"4dff";s:12:"ext_icon.gif";s:4:"04c6";s:17:"ext_localconf.php";s:4:"1a51";s:14:"ext_tables.php";s:4:"777a";s:14:"ext_tables.sql";s:4:"d41d";s:47:"Classes/Controller/RecommendationController.php";s:4:"65a7";s:56:"Classes/Controller/ReturnRecommendationUrlController.php";s:4:"3398";s:39:"Classes/Domain/Model/Recommendation.php";s:4:"37ae";s:52:"Classes/Domain/Validator/RecommendationValidator.php";s:4:"0565";s:27:"Classes/Hook/ClearCache.php";s:4:"eee9";s:24:"Classes/Utility/Mail.php";s:4:"c09a";s:28:"Classes/Utility/Settings.php";s:4:"87e1";s:51:"Configuration/FlexForms/flexform_recommendation.xml";s:4:"d8dc";s:34:"Configuration/TypoScript/setup.txt";s:4:"4260";s:40:"Resources/Private/Language/locallang.xml";s:4:"1b80";s:49:"Resources/Private/Language/locallang_flexform.xml";s:4:"53f6";s:38:"Resources/Private/Layouts/default.html";s:4:"9325";s:35:"Resources/Private/Layouts/mail.html";s:4:"3562";s:42:"Resources/Private/Partials/formErrors.html";s:4:"57c9";s:64:"Resources/Private/Templates/Recommendation/MailTemplateHtml.html";s:4:"6f88";s:64:"Resources/Private/Templates/Recommendation/MailTemplateText.html";s:4:"c15c";s:56:"Resources/Private/Templates/Recommendation/ShowForm.html";s:4:"4615";s:65:"Resources/Private/Templates/Recommendation/ShowMailSendError.html";s:4:"405b";s:14:"doc/manual.sxw";s:4:"de33";}',
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:21:"ext_conf_template.txt";s:4:"4dff";s:12:"ext_icon.gif";s:4:"04c6";s:17:"ext_localconf.php";s:4:"1a51";s:14:"ext_tables.php";s:4:"777a";s:14:"ext_tables.sql";s:4:"d41d";s:47:"Classes/Controller/RecommendationController.php";s:4:"65a7";s:56:"Classes/Controller/ReturnRecommendationUrlController.php";s:4:"3398";s:39:"Classes/Domain/Model/Recommendation.php";s:4:"37ae";s:52:"Classes/Domain/Validator/RecommendationValidator.php";s:4:"0565";s:27:"Classes/Hook/ClearCache.php";s:4:"eee9";s:24:"Classes/Utility/Mail.php";s:4:"c09a";s:28:"Classes/Utility/Settings.php";s:4:"87e1";s:51:"Configuration/FlexForms/flexform_recommendation.xml";s:4:"d8dc";s:34:"Configuration/TypoScript/setup.txt";s:4:"4260";s:40:"Resources/Private/Language/locallang.xml";s:4:"1b80";s:49:"Resources/Private/Language/locallang_flexform.xml";s:4:"53f6";s:38:"Resources/Private/Layouts/default.html";s:4:"9325";s:35:"Resources/Private/Layouts/mail.html";s:4:"3562";s:42:"Resources/Private/Partials/formErrors.html";s:4:"57c9";s:64:"Resources/Private/Templates/Recommendation/MailTemplateHtml.html";s:4:"6f88";s:64:"Resources/Private/Templates/Recommendation/MailTemplateText.html";s:4:"c15c";s:56:"Resources/Private/Templates/Recommendation/ShowForm.html";s:4:"4af5";s:65:"Resources/Private/Templates/Recommendation/ShowMailSendError.html";s:4:"405b";s:14:"doc/manual.sxw";s:4:"de33";}',
 );
 
 ?>
